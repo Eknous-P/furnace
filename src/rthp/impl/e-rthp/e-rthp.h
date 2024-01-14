0f9eb94f00cfdeb7a355c00456a7ce12431fc910
Eknous-P <ehovakimyan053@gmail.com>
2024-01-14 13:14:08 +0400

    remove useless stuff

--- src/rthp/impl/e-rthp/e-rthp.h
+++ src/rthp/impl/e-rthp/e-rthp.h
@@ -7,10 +7,6 @@
 #include <vector>
 #include "serial/serial.h"
 
-#define MSG_CONFIRM "^"
-#define MSG_CANCEL "X"
-#define MSG_BEACON "*"
-
 class ERTHP {
   serial::Serial serialPort;
   struct ERTHP_Serial {
@@ -18,13 +14,11 @@ class ERTHP {
 
     unsigned long int serialBaudrate, serialTimeout;
     std::string portName;
-    unsigned long int beaconInterval;
 
     ERTHP_Serial():
       serialBaudrate(9600),
       serialTimeout(1000),
-      portName(""),
-      beaconInterval(1000) {}
+      portName("") {}
   } erthp_serial;
   struct ERTHP_Log {
     std::string logBuffer[64]; // cyclical buffer, not intended as a main log buffer
@@ -44,7 +38,6 @@ class ERTHP {
 
     int initSerial(std::string port, unsigned int baudrate, unsigned int timeout);
     int sendSerial(std::string msg);
-    void sendBeacon();
     void closeSerial();
 
     // logging
