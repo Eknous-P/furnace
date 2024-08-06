1c94de7513cb03462f0fa4e6a7a1dbcc9d7bb079
Eknous-P <ehovakimyan053@gmail.com>
2024-08-06 13:57:48 +0400

    m64 export: cleanup, chan data pointers

--- src/engine/m64Ops.cpp
+++ src/engine/m64Ops.cpp
@@ -37,22 +37,57 @@ SafeWriter* DivEngine::saveM64(unsigned char volumeScale) {
   SafeWriter* w=new SafeWriter;
   w->init();
 
-  // note to self: .m64 is BIG ENDIAN
+  unsigned char channels=getSystemDef(song.system[0])->channels;
+  unsigned short* chanData=new unsigned short[channels]; // list of channel data pointers
+  memset(chanData,0,2*channels);
+  unsigned short chanDataBegin; // pointer to channel data pointers
 
+  /* # note to self: .m64 is BIG ENDIAN */
+
+  /* .m64 "header"? (sequence data ~ global setup) */
+
+  // maybe make these a param?
   w->write("\xd3\x20",2); // mute behavior -> lower volume
   w->write("\xd5\x3f",2); // mute mode multiplier -> 3F
 
-  unsigned short chanMask = (1<<getSystemDef(song.system[0])->channels)-1;
-  w->write("\xd7",1);
+  unsigned short chanMask = (1<<channels)-1;
+  w->writeC(0xd7);
   w->writeS_BE(chanMask); // init channels
 
-  w->write("\xdb",1);
+  w->writeC(0xdb);
   w->writeC(volumeScale); // set vol scale
 
-  w->write("\xdd",1);
+  w->writeC(0xdd);
   w->writeC((unsigned char)(curSubSong->hz*2.5)); // set song tempo
+  chanDataBegin = w->tell();
+  // write temporary channel data pointers
+  for (unsigned char i=0; i<channels; i++) {
+    w->writeC(0x90+i);
+    w->writeS_BE(0); // pad with 0s
+  }
 
-  w->write("\xff",1); // end sequence
+  /* channel data ~ orders*/
+
+  for (unsigned char i=0; i<channels; i++) {
+    chanData[i] = w->tell()+1;
+  }
+
+  /* layer/track data ~ patterns*/
+
+
+  /* rewrite channel data pointers */
+  // go back
+  w->seek(chanDataBegin,SEEK_SET);
+  // write correct pointers
+  for (unsigned char i=0; i<channels; i++) {
+    w->writeC(0x90+i);
+    w->writeS_BE(chanData[i]);
+  }
+
+  w->writeC(0xff); // end sequence
+
+  delete[] chanData;
+  chanData=NULL;
   
   remainingLoops=-1;
   playing=false;
